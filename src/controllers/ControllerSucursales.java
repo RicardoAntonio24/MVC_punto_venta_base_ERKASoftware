cdb9af89551dd6f74053b2b3485e231d84728e1e
RicardoAntonio24 <39196878+RicardoAntonio24@users.noreply.github.com>
2018-11-13 20:18:50 -0600

    botones de inserscion y modificacion de registros en sucursales funcionando

--- src/controllers/ControllerSucursales.java
+++ src/controllers/ControllerSucursales.java
@@ -81,7 +81,22 @@ public class ControllerSucursales {
         viewSucursales.jcb_estado.setSelectedItem(modelSucursales.getEstado());
         viewSucursales.jtf_ciudad.setText(modelSucursales.getCiudad());      
             }
+     private void initComponents() {
+                viewSucursales.setLocationRelativeTo(null);
+        viewSucursales.setTitle("Sucursales ACME");
+        viewSucursales.setVisible(true);
+            }
+        private void setActionListener() {
+               viewSucursales.jb_primero.addActionListener(actionListener);
+        viewSucursales.jb_anterior.addActionListener(actionListener);
+        viewSucursales.jb_siguiente.addActionListener(actionListener);
+        viewSucursales.jb_ultimo.addActionListener(actionListener);
         
+        viewSucursales.jb_nuevo.addActionListener(actionListener);
+        viewSucursales.jb_insertar.addActionListener(actionListener);
+        viewSucursales.jb_modificar.addActionListener(actionListener);
+        viewSucursales.jb_eliminar.addActionListener(actionListener);
+            }
 
             private void jb_primero_actionPerformed() {
                  modelSucursales.moverPrimerRegistro();
@@ -104,8 +119,6 @@ public class ControllerSucursales {
             }
 
             private void jb_nuevo_actionPerformed() {
-                
-        viewSucursales.jtf_idsucursal.setText("");
         viewSucursales.jtf_nombre.setText("");
         viewSucursales.jtf_telefono.setText("");
         viewSucursales.jtf_calle.setText("");
@@ -116,32 +129,33 @@ public class ControllerSucursales {
             }
 
             private void jb_insertar_actionPerformed() {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         // Asigna los valores introducidos en la vista a las variables del modelo. (13)
+        modelSucursales.setNombre(viewSucursales.jtf_nombre.getText());
+        modelSucursales.setTelefono(viewSucursales.jtf_telefono.getText());
+        modelSucursales.setCalle(viewSucursales.jtf_calle.getText());
+        modelSucursales.setColonia(viewSucursales.jtf_colonia.getText());
+        modelSucursales.setCod_postal(viewSucursales.jtf_codigopostal.getText());
+        modelSucursales.setCiudad(viewSucursales.jtf_ciudad.getText());
+        modelSucursales.setEstado((String) viewSucursales.jcb_estado.getSelectedItem());
+        modelSucursales.insertarRegistro(); // Invoca al método para Guardar o Insertar un nuevo registro en la tabla proveedores.
             }
 
             private void jb_modificar_actionPerformed() {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
-
-            private void setActionListener() {
-               viewSucursales.jb_primero.addActionListener(actionListener);
-        viewSucursales.jb_anterior.addActionListener(actionListener);
-        viewSucursales.jb_siguiente.addActionListener(actionListener);
-        viewSucursales.jb_ultimo.addActionListener(actionListener);
-        
-        viewSucursales.jb_nuevo.addActionListener(actionListener);
-        viewSucursales.jb_insertar.addActionListener(actionListener);
-        viewSucursales.jb_modificar.addActionListener(actionListener);
-        viewSucursales.jb_eliminar.addActionListener(actionListener);
+        modelSucursales.setNombre(viewSucursales.jtf_nombre.getText());
+        modelSucursales.setTelefono(viewSucursales.jtf_telefono.getText());
+        modelSucursales.setCalle(viewSucursales.jtf_calle.getText());
+        modelSucursales.setColonia(viewSucursales.jtf_colonia.getText());
+        modelSucursales.setCod_postal(viewSucursales.jtf_codigopostal.getText());
+        modelSucursales.setCiudad(viewSucursales.jtf_ciudad.getText());
+        modelSucursales.setEstado((String) viewSucursales.jcb_estado.getSelectedItem());
+        modelSucursales.modificarRegistro(); // Invoca al método para Modificar (actualizar) un registro.
             }
 
             
 
-            private void initComponents() {
-                viewSucursales.setLocationRelativeTo(null);
-        viewSucursales.setTitle("Sucursales ACME");
-        viewSucursales.setVisible(true);
-            }
+            
+
+           
 
     private void setValues() {
          String id_sucursal = Integer.toString(modelSucursales.getId_sucursal());
--- src/models/ModelSucursales.java
+++ src/models/ModelSucursales.java
@@ -92,7 +92,7 @@ public class ModelSucursales {
 
     public void conectarDB() {
         try {
-            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3307/ferreteria_acme", "root", "");
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/ferreteria_acme", "root", "");
             st = conexion.createStatement();
             String sql = "SELECT * FROM sucursales;";
             rs = st.executeQuery(sql);
@@ -216,70 +216,54 @@ public class ModelSucursales {
         }
     }
 
-//    /**
-//     * Método que realiza las siguiente acciones: - Crea un nuevo registro y lo
-//     * almacena en la base de datos.
-//     */
-//    public void insertarRegistro() {
-//        try {
-//            tipo_cliente = this.getTipo_cliente(); // Obtiene los valores actuales de las variables. (16)
-//            id_sucursal = rs.getInt("id_sucursal");
-//
-//                nombre = rs.getString("nombre_sucursal");
-//                telefono = rs.getString("telefono_sucursal");
-//                calle = rs.getString("calle_numero_sucursal");
-//                colonia = rs.getString("colonia_sucursal");
-//                cod_postal = rs.getString("cod_postal_sucursal");
-//                ciudad = rs.getString("ciudad_sucursal");
-//                estado = rs.getString("estado_sucursal");
-//            
-//            st.executeUpdate("INSERT INTO sucursales (tipo_sucursal, nombre_sucursal, telefono_sucursal, calle_numero_sucursal, colonia_sucursal, cod_postal_sucursal, ciudad_sucursal, estado_sucursal)"
-//                    + " VALUES ('" + tipo_sucursal + "','""', '" + nombre + "', '" + "', '" + "', '" + telefono + "', '"+ "', '" + calle + "', '" + colonia + "', '" + cod_postal + "', '" + ciudad + "', '" + estado); ");
-//            JOptionPane.showMessageDialog(null, "Registro guardado.");
-//            this.conectarDB();
-//            this.moverUltimoRegistro();
-//        } catch (SQLException err) {
-//            JOptionPane.showMessageDialog(null, "Error " + err.getMessage());
-//        }
-//    }
-//
-//    /**
-//     * Método que realiza las siguiente acciones:
-//     *  - Guarda (actualiza) los cambios realizados a un registro seleccionado.
-//     */
-//    public void modificarRegistro() {
-//        try {
-//            id_sucursal = rs.getInt("id_sucursal");
-//
-//                nombre = rs.getString("nombre_sucursal");
-//                telefono = rs.getString("telefono_sucursal"); 
-//                calle = rs.getString("calle_numero_sucursal");
-//                colonia = rs.getString("colonia_sucursal");
-//                cod_postal = rs.getString("cod_postal_sucursal");
-//                ciudad = rs.getString("ciudad_sucursal");
-//                estado = rs.getString("estado_sucursal");
-//            
-//            st.executeUpdate("UPDATE clientes SET tipo_cliente = '"+ tipo_sucursal +"', rfc_cliente = '"+ rfc +"', "
-//                    + "nombre_sucursal = '"+ nombre +"', apellido_pat_cliente = '"+ apellido_pat +"', "
-//                        
-//                            
-//                                + "colonia_sucursal = '"+ colonia +"', cod_postal_sucursal = '"+ cod_postal +"', "
-//                                    + "ciudad_sucursal = '"+ ciudad +"', estado_sucursal = '"+ estado +"', "
-//                                        + "nombre_sucursal = '"+ nom_sucursal +"', "
-//                                            
-//                                                
-//                                                    + "telefono_sucursal = '"+ telefono_sucursal +"' "
-//                                                        + "WHERE id_sucursal = "+ id_sucursal +"; ");
-//            JOptionPane.showMessageDialog(null, "Se ha modificado el registro.");
-//            this.conectarDB();
-//            this.moverUltimoRegistro();
-//        }
-//        catch(SQLException err) { 
-//            JOptionPane.showMessageDialog(null,"Error "+err.getMessage()); 
-//        }
-//    }
-//  
+    /**
+     * Método que realiza las siguiente acciones: - Crea un nuevo registro y lo
+     * almacena en la base de datos.
+     */
+    public void insertarRegistro() {
+        try {
+             // Obtiene los valores actuales de las variables. 
+                nombre = this.getNombre();
+                telefono = this.getTelefono();
+                calle = this.getCalle();
+                colonia = this.getColonia();
+                cod_postal = this.getCod_postal();
+                ciudad = this.getCiudad();
+                estado = this.getEstado();
+            
+            st.executeUpdate("INSERT INTO sucursales (nombre_sucursal, telefono_sucursal, calle_numero_sucursal, colonia_sucursal, cod_postal_sucursal, ciudad_sucursal, estado_sucursal)"
+             + " VALUES ('" + nombre + "', '" + telefono + "', '" + calle + "', '" + colonia + "', '" + cod_postal + "', '" + ciudad + "', '" + estado + "'); ");       
+            JOptionPane.showMessageDialog(null, "Registro guardado.");
+            this.conectarDB();
+            this.moverUltimoRegistro();
+        } catch (SQLException err) {
+            JOptionPane.showMessageDialog(null, "Error " + err.getMessage());
+        }
+    }
 
-    
-
+    /**
+     * Método que realiza las siguiente acciones:
+     *  - Guarda (actualiza) los cambios realizados a un registro seleccionado.
+     */
+    public void modificarRegistro() {
+        try {
+            id_sucursal = rs.getInt("id_sucursal");
+             nombre = this.getNombre();
+                telefono = this.getTelefono();
+                calle = this.getCalle();
+                colonia = this.getColonia();
+                cod_postal = this.getCod_postal();
+                ciudad = this.getCiudad();
+                estado = this.getEstado();
+            
+            st.executeUpdate("UPDATE sucursales SET nombre_sucursal = '" + nombre + "', telefono_sucursal = '" + telefono + "', calle_numero_sucursal = '" + calle + "', colonia_sucursal = '" + colonia + "', cod_postal_sucursal = '" + cod_postal + "', ciudad_sucursal = '" + ciudad + "', estado_sucursal = '" + estado +"' WHERE id_sucursal = "+ id_sucursal +"; ");
+            JOptionPane.showMessageDialog(null, "Se ha modificado el registro.");
+            this.conectarDB();
+            this.moverUltimoRegistro();
+        }
+        catch(SQLException err) { 
+            JOptionPane.showMessageDialog(null,"Error "+err.getMessage()); 
+        }
+    }
 }
+ 
